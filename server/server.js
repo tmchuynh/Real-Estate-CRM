0ae28e1b532098d35889894a8d4658921c2df88f
tmchuynh <tinamchuynh@gmail.com>
2023-04-17 18:43:36 -0700

    🎉 feat(client, server): add new components and remove dog router ✨ feat(client): add DynamicTable component ✨ feat(client): add SideNav component ✨ feat(client): add LoginReg component ✨ feat(server): remove dog router The DynamicTable component is added to the client, which allows for the creation of dynamic tables. The SideNav component is added to the client, which provides a navigation menu. The LoginReg component is added to the client, which provides a login and registration form. The dog router is removed from the server as it is no longer needed.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import dbConnect from './config/mongoose.config.js';
-import dogRouter from './routes/dog-routes.js';
 import cors from 'cors';
 
 const app = express();
 dotenv.config();
 
 app.use(express.json(), cors());
-app.use('/api/dogs', dogRouter);
 
 app.get('/api/message', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
