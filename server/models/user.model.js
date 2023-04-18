2968729076d540050e4ac0a1b839141efe521991
Gray-Desktop <gray.boulware@gmail.com>
2023-04-17 22:23:28 -0500

    🎉 feat(user.model.js): add UserSchema with validations 📦 chore(package.json): add mongoose-type-email dependency The UserSchema now includes validations for email, password, fullName, location, title, and profilePic. The email field is now of type mongoose.SchemaTypes.Email, which auto-validates using an email regex. The password field now has a minimum length of 8 characters. The package.json file now includes the mongoose-type-email dependency, which is used for email validation.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,1 +1,38 @@
-//CRM users model
+const mongoose = require('mongoose');
+//create the author schema with validations
+const UserSchema = new mongoose.Schema({
+    //mongoose added Email datatype that auto validates using an email regex
+    email: {
+        type: mongoose.SchemaTypes.Email,
+        correctTld: true,
+        index: true,
+        required: [true, 'Email is required!'],
+        minLength: [5, 'Email must be at least 5 characters long!']
+    },
+    password : {
+        type: String,
+        match: [],
+        required: [true, 'Password is required!'],
+        minLength: [8, 'Password must be at least 8 characters.']
+    },
+    fullName: {
+        type: String,
+        required: false
+    },
+    location: {
+        type: String,
+        required: false
+    },
+    title: {
+        type: String,
+        required: false
+    },
+    profilePic: {
+        //will store as Base64 encoded string
+        type: String,
+        required: false
+    }
+});
+//export the author as a mongoose model
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
