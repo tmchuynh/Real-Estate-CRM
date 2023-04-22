c12470a87ead417384d386e15170aab5c17930cb
tmchuynh <tmchuynh@gmail.com>
2023-04-21 20:10:44 -0700

    🔨 refactor(LeadPopUp.jsx): rename Modal component to CustomModal The Modal component was renamed to CustomModal to avoid naming conflicts with the imported Modal component from react-bootstrap.

--- client/src/components/LeadPopUp.jsx
+++ client/src/components/LeadPopUp.jsx
@@ -1,42 +1,31 @@
 import { Modal, Form } from 'react-bootstrap';
 
-function Modal(props) {
-    function handleSubmit() {
-        console.log("form submitted")
-    }
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Would You Like to Speak with an Agent?
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
+function CustomModal(props) {
+  const { title, body, onSubmit, onHide } = props;
 
-                <Form.Label htmlFor="inputPassword5">Name</Form.Label>
-                <Form.Control
-                    type="text"
-                    id="fullName"
-                    aria-describedby="fullName"
-                />
-                <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    id="inputPassword5"
-                    aria-describedby="passwordHelpBlock"
-                />
-                <Button onClick={props.onHide}>Close</Button>
-                <Button>Send</Button>
-                </Form>
-            </Modal.Body>
-        </Modal>
-    );
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (onSubmit) {
+      onSubmit();
+    }
+  }
+
+  return (
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {body}
+          <Button onClick={onHide}>Close</Button>
+          <Button type="submit">Send</Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
-export default Modal;
+export default CustomModal;
