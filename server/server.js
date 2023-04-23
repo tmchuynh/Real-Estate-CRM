82c2d3cbc52831e74c58b896a3fdfa4bacb768ec
tmchuynh <tmchuynh@gmail.com>
2023-04-23 10:46:35 -0700

    🔧 chore(mongoose.config.js): remove unnecessary import statement 🔧 chore(package.json): add type module and nodemon as dev dependency 🔧 chore(server.js): use ES6 import statements and update middleware functions The import statement for mongoose.config.js was unnecessary and has been removed. The package.json file has been updated to include "type": "module" and nodemon as a dev dependency. The server.js file has been updated to use ES6 import statements and middleware functions have been updated to use json() and urlencoded() instead of express.json() and express.urlencoded().

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+import express, { json, urlencoded } from 'express';
+import cors from 'cors';
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
-require('./config/mongoose.config');
+import '../server/config/mongoose.config.js';
 //REQUIRED FOR CRUD
-app.use(express.json(), cors(), express.urlencoded({ extended: true }));
+app.use(json(), cors(), urlencoded({ extended: true }));
 //ADD ALL MODEL ROUTES HERE
 require('./routes/user.routes')(app);
 require('./routes/lead.routes')(app);
