b3e25f33b7839d8938427b203df1947d414cf3aa
tmchuynh <tmchuynh@gmail.com>
2023-04-22 19:05:26 -0700

    🚀 chore(client): add fontawesome dependencies and refactor SideNav component The FontAwesomeIcon component is imported from the '@fortawesome/react-fontawesome' package and used to display icons. The '@fortawesome/fontawesome-svg-core' package is also added as a dependency. The SideNav component is refactored to use the FontAwesomeIcon component to display icons for the Home and Lead links. The class attribute is replaced with the className attribute to avoid warnings.

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import { Nav, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 const SidebarNav = () => {
   return (
-    <div class="btn-group-vertical">
-      <Button type="button" class="btn btn-primary">
-        <Nav.Link href="/">Home</Nav.Link>
-      </Button>
-      <Button type="button" class="btn btn-primary">
-        <Nav.Link href="/lead_profile">Lead</Nav.Link>
-      </Button>
-      <Button type="button" class="btn btn-primary">
-        <Nav.Link href="/user_profile">User</Nav.Link>
-      </Button>
-      <Button type="button" class="btn btn-primary">
-        <Nav.Link href="#">Documents</Nav.Link>
-      </Button>
-      <Button type="button" class="btn btn-primary">
-        <Nav.Link href="#">Leads</Nav.Link>
-      </Button>
-      <Button type="button" class="btn btn-primary">
-        <Nav.Link href="#">Contact</Nav.Link>
-      </Button>
+    <div class="btn-group-vertical justify-content-start">
+      <Nav.Link href="/">
+        <Button type="button" class="btn btn-primary">
+          <FontAwesomeIcon icon={faHouse} />
+          Home
+        </Button>
+      </Nav.Link>
+
+      <Nav.Link href="/lead_profile">
+        <Button type="button" class="btn btn-primary">
+          <FontAwesomeIcon icon={faUsers} />
+          Lead
+        </Button>
+      </Nav.Link>
     </div>
   );
 };
