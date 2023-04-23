b1afe74e5343d95dbb5cf643d582549f153747e8
gray boulware <gray.boulware@gmail.com>
2023-04-22 20:34:18 -0700

    add

--- server/server.js
+++ server/server.js
@@ -3,11 +3,10 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
-require('./config/mongoose.config');
+require('./server/config/mongoose.config');
 //REQUIRED FOR CRUD
 app.use(express.json(), cors(), express.urlencoded({ extended: true }));
 //ADD ALL MODEL ROUTES HERE
-require('./routes/user.routes')(app);
-require('./routes/lead.routes')(app);
+require('./server/routes/author.routes')(app);
 //THIS SHOULD BE LAST
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
+app.listen(port, () => console.log(`Listening on port: port`) );
