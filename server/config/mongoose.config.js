9045b735e7c6342a4f298615ed5c2f5e086f800e
tmchuynh <tmchuynh@gmail.com>
2023-04-23 11:17:28 -0700

    🔥 chore(mongoose.config.js): remove unused export statement The export statement at the end of the file is not used and can be safely removed.

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -11,5 +11,3 @@ connect(uri, {
 })
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
-
-export default dbConnect;
