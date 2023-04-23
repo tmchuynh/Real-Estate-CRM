d56e104e87b68121425d7ee26d9e1a47bc189fcd
tmchuynh <tmchuynh@gmail.com>
2023-04-23 11:21:23 -0700

    🔥 chore(Retool.jsx): remove Retool component The Retool component has been removed from the project.

--- client/src/components/Retool.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const Retool = () => {
-    return ( 
-        <iframe src="https://tmchuynh.retool.com/apps/b2b%20crm" width="100%" height="800px"></iframe>
-    )
-}
-export default Retool
