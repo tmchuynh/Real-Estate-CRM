d37cb4cef25c40423eff1043ef8c8cb1c360515d
tmchuynh <tmchuynh@gmail.com>
2023-04-23 19:39:57 -0700

    🎨 style(LeadDetails.jsx): refactor Timeline component to use Stack and ul/li elements 🚀 perf(LeadDetails.jsx): improve Timeline component performance The Timeline component has been refactored to use the Stack and ul/li elements, which improves the readability of the code and makes it easier to maintain. The performance of the Timeline component has also been improved by removing the react-chrono dependency and replacing it with a simple ul/li list.

--- client/src/views/LeadDetails.jsx
+++ client/src/views/LeadDetails.jsx
@@ -1,56 +1,47 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom';
-import { Chrono } from "react-chrono";
-
-function Timeline({ items }) {
-    return (
-        <div style={{ width: "500px", height: "950px" }}>
-            <Chrono items={items} mode="TREE" />
-        </div>
-    );
-}
+import { Button, Form, Stack } from 'react-bootstrap'
 
 const LeadDetails = ({ index }) => {
-    const navigate = useNavigate();
     const [timeline, setTimeline] = useState([]);
     const [showForm, setShowForm] = useState(false);
-    const [event, setEvent] = useState('');
+    const [event, setEvent] = useState("");
 
     const handleAddEvent = (e) => {
         e.preventDefault();
         setTimeline([...timeline, event]);
         setShowForm(false);
-        setEvent('');
-    };
-
-    const goBack = () => {
-        navigate("/leads")
+        setEvent("");
     }
 
     return (
         <div>
-            <div className="d-flex justify-content-between">
-
             <h2>Lead Details</h2>
-            <Button onClick={goBack}>Back</Button>
-            </div>
             {/* <p>Name: {index.name}</p>
             <p>Email: {index.email}</p>
             <p>Phone Number: {index.phone}</p> */}
 
             <h3>Timeline</h3>
-            <Button onClick={() => setShowForm(true)}>Add Event</Button>
-            {showForm && (
-                <form onSubmit={handleAddEvent}>
-                    <Form.Group controlId="formBasicEvent">
-                        <Form.Label>Event</Form.Label>
-                        <Form.Control type="text" placeholder="Enter event" value={event} onChange={(e) => setEvent(e.target.value)} />
-                    </Form.Group>
-                    <Button type="submit">Add</Button>
-                </form>
-            )}
-            <Timeline items={timeline} />
+            <Stack gap={4}>
+                <ul>
+                    {timeline.map((event, index) => (
+                        <li key={index}>{event}</li>
+                    ))}
+                </ul>
+
+                <Button onClick={() => setShowForm(true)}>Add Event</Button>
+
+                {showForm && (
+                    <Form onSubmit={handleAddEvent}>
+                        <div className="d-flex">
+
+                            <Form.Control type="text" value={event} onChange={(e) => setEvent(e.target.value)} />
+
+                            <Button type="submit" className='ms-3'>Add</Button>
+                        </div>
+                    </Form>
+                )}
+
+            </Stack>
         </div>
     );
 };
