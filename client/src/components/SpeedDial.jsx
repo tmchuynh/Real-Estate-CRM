96c86165231adf4a1b61b06769246a046d273400
tmchuynh <tinamchuynh@gmail.com>
2023-04-26 07:37:20 -0700

    🎨 style(SpeedDial.jsx): rename actions prop to buttons The actions prop has been renamed to buttons to better reflect the purpose of the prop. This change improves the readability of the code and makes it easier to understand the purpose of the prop.

--- client/src/components/SpeedDial.jsx
+++ client/src/components/SpeedDial.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Box, SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
 
-const BasicSpeedDial = ({actions}) => {
+const BasicSpeedDial = ({buttons}) => {
   return (
     <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
       <SpeedDial
@@ -9,11 +9,11 @@ const BasicSpeedDial = ({actions}) => {
         sx={{ position: 'absolute', bottom: 16, right: 16 }}
         icon={<SpeedDialIcon />}
       >
-        {actions.map((action) => (
+        {buttons.map((button) => (
           <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
+            key={button.name}
+            icon={button.icon}
+            tooltipTitle={button.name}
           />
         ))}
       </SpeedDial>
