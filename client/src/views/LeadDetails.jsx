24c7f4fabd256257fa1464ba1c63dd667437df50
tmchuynh <tinamchuynh@gmail.com>
2023-04-27 08:04:39 -0700

    🔧 chore(client): update react-scripts to version ^5.0.1 🚀 feat(client): add TimelineForm component to LeadDetails view The update to react-scripts is a routine maintenance task to keep the project up to date. The addition of the TimelineForm component to the LeadDetails view allows users to add events to the timeline of a lead.

--- client/src/views/LeadDetails.jsx
+++ client/src/views/LeadDetails.jsx
@@ -11,7 +11,7 @@ const LeadDetails = (props) => {
     const [timeline, setTimeline] = useState([]);
     const [showForm, setShowForm] = useState(false);
     const [event, setEvent] = useState("");
-    const {firstName, lastName, email, phoneNumber, status, buying, selling, marketArea } = props.lead;
+    const { firstName, lastName, email, phoneNumber, status, buying, selling, marketArea } = props.lead;
     console.log(firstName);
 
 
@@ -105,7 +105,7 @@ const LeadDetails = (props) => {
                     </div>
 
 
-                            <TimelineForm/>
+                    <TimelineForm />
 
 
 
